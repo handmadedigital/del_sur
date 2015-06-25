9507ea0714b719bf1d8f3c57f6cb383d7ad48a10
rod <rodz@rod.rod>
2015-06-25 10:13:54 -0400

    design chnages

--- app/router.js
+++ app/router.js
@@ -11,6 +11,20 @@ Router.map(function() {
   this.route('catalog');
   this.route('blogs');
   this.route('contact');
+  this.route('admin', function() {
+    this.route('login');
+    this.route('dashboard');
+    this.route('blogs', function() {
+      this.route('new');
+      this.route('update');
+      this.route('delete');
+    });
+    this.route('recipes', function() {
+      this.route('new');
+      this.route('delete');
+      this.route('update');
+    });
+  });
 });
 
 export default Router;
---
+++ tests/unit/routes/admin/recipes/new-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:admin/recipes/new', 'Unit | Route | admin/recipes/new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
